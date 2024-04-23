608ffa56c9b2ea7806377cb0999273a01273b3e1
Rommel <giostrasser@gmail.com>
2024-04-23 09:23:40 -0300

    Inicia a tentativa de conversão de string para intenger
    Ainda não terminamos!

--- main.c
+++ main.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define VECTORSIZE 1000
+#define VECTORMAXSIZE 1000
 
 FILE *fileread;
 FILE *filewrite;
 
 int main(){
 
-    fileread = fopen("input.txt", "r");
+    fileread = fopen("rawInput.txt", "r");
     filewrite = fopen("output.txt", "w"); 
 
-    char input[VECTORSIZE];
+    char rawInput[VECTORMAXSIZE];
 
+    int lineCount  = 0;
     while(!feof(fileread)){
-        fgets(input, VECTORSIZE, fileread);
-        fprintf(filewrite, input);
-        printf("%s", input);
+        fgets(rawInput, VECTORMAXSIZE, fileread);
+        fprintf(filewrite, rawInput);
+        printf("%s", rawInput);
+        lineCount++;
     }
 
-    printf("%d", sizeof(input));
+    int input[lineCount];
+
+    for(int i = 0; i < lineCount; i++){
+        char temp[sizeof(rawInput[i])] = rawInput[i];
+        input[i] = atoi(temp);
+        printf("%d\n", input[i]);
+    }
+
+    //printf("%d", sizeof(rawInput));
+    printf("%d", lineCount);
 
     return 0;
 }
