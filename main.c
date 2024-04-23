92fa27b48e1110f2228f610a81b4c076c0b86747
Rommel <giostrasser@gmail.com>
2024-04-23 08:45:02 -0300

    Inicia trabalho
    :)

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+
+#define VECTORSIZE 1000
+
+FILE *file;
 
 int main(){
-    
+    file = fopen("input.txt", "r");
+
+    char input[VECTORSIZE];
+
+    while(!feof(file)){
+        fgets(input, VECTORSIZE, file);
+    }
+
+    printf("%s", input);
 
     return 0;
 }
