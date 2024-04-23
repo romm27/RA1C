54daac6fbd87e42371cc46eed52cc34ab9152476
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-04-23 20:07:24 -0300

    Refaz a lógica para leitura de duas linhas apenas.

--- main.c
+++ main.c
@@ -2,36 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINES 1000
-#define MAX_LENGTH 100
+#define MAX 1000
+#define MAX_LENGTH 1000
 
 int main() {
+
     FILE *fileRead;
     FILE *fileWrite;
 
     char line[MAX_LENGTH];
-    char *lines[MAX_LINES];
-    int count = 0;
+    char *rawInput[MAX];
+    int integerQuantity = 0;
 
     fileRead = fopen("input.txt", "r");
     fileWrite = fopen("output.txt", "w");
 
-    while (fgets(line, sizeof(line), fileRead) != NULL) {
-
-        if (line[strlen(line) - 1] == '\n') {
-            line[strlen(line) - 1] = '\0';
-        }
-
-        lines[count] = malloc(strlen(line) + 1);
-        strcpy(lines[count], line);
-        fprintf(fileWrite, line);
-        count++;
-
-        if (count >= MAX_LINES) {
-            printf("Limite máximo de linhas atingido.\n");
-            break;
-        }
+    if (fileRead == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
     }
+
+    if (fgets(line, sizeof(line), fileRead) != NULL) {
+        integerQuantity = atoi(line);
+    }
+
+    if (fgets(line, sizeof(line), fileRead) != NULL) {
+        printf("\n %s", line);
+    }
+
+}
+
+
+
+
+
+
+
+
+    // while (fgets(line, sizeof(line), fileRead) != NULL) {
+
+    //     // if (line[strlen(line) - 1] == '\n') {
+    //     //     line[strlen(line) - 1] = '\0';
+    //     // }
+        
+    //     rawInput[count] = malloc(strlen(line) + 1);
+    //     strcpy(rawInput[count], line);
+    //     fprintf(fileWrite, line);
+    //     count++;
+
+    //     if (count >= MAX) {
+    //         printf("Limite máximo de linhas atingido.\n");
+    //         break;
+    //     }
+    // }
 
     fclose(fileRead);
     fclose(fileWrite);
